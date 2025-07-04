3d157cd80a0b4713225e3a06ffdd484dcf063539
Varun MK <varunshettymk2003@gmail.com>
2025-07-04 09:29:02 +0530

    Add files via upload

---
+++ src/main/java/com/jsp/springboot/employeedb/SpringBootEmployeeDbApplication.java
@@ -0,0 +1,13 @@
+package com.jsp.springboot.employeedb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEmployeeDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEmployeeDbApplication.class, args);
+	}
+
+}
