7d2748f4c75b05ae30382eaab23825b075039033
Varun MK <varunshettymk2003@gmail.com>
2025-07-04 09:21:40 +0530

    Add files via upload

---
+++ src/main/java/com/jsp/springboot/employeedb/entity/Employee.java
@@ -0,0 +1,55 @@
+package com.jsp.springboot.employeedb.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Employee {
+
+	@Id
+	private int employeeId;
+	private String employeeName;
+	private String email;
+	private int salary;
+	private int experience;
+
+	public int getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getEmployeeName() {
+		return employeeName;
+	}
+
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	public int getExperience() {
+		return experience;
+	}
+
+	public void setExperience(int experience) {
+		this.experience = experience;
+	}
+}
